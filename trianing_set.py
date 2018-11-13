cd7d597eb399bb1ffa0128bdfef043e3b61db031
Arun <arunv4080@gmail.com>
2018-11-13 23:07:38 +0530

    update

--- trianing_set.py
+++ trianing_set.py
@@ -1,21 +1,22 @@
+import glob
 import os
 import cv2
 import numpy as np
 from PIL import Image
 
-EigenFace=cv2.createEigenFaceRecognizer(15)
-FisherFace=cv2.createFisherFaceRecognizer(5)
-LBPHFace=cv2.createLBPHFaceRecognizer(2, 2, 7, 7)
+EigenFace=cv2.face.EigenFaceRecognizer_create(15)
+FisherFace=cv2.face.FisherFaceRecognizer_create(5)
+LBPHFace=cv2.face.LBPHFaceRecognizer_create(2, 2, 7, 7)
 path='dataset'
 
 def getImagesWithID(path):
-    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
+    imagePaths=glob.glob('dataset/*.jpg')
     faces=[]
     IDs=[]
     for imagePath in imagePaths:
-        faceImg=Image.open(imagePath).convert('L');
-	faceImg=faceImg.resize((110,110))        
-	faceNp=np.array(faceImg,'uint8')
+        print(imagePath)
+        faceImg=Image.open(imagePath).convert('L')
+        faceNp=np.array(faceImg,'uint8')
         ID=int(os.path.split(imagePath)[-1].split('.')[1])
         faces.append(faceNp)
         IDs.append(ID)
@@ -27,9 +28,6 @@ Ids,faces=getImagesWithID(path)
 print('Training.......')
 EigenFace.train(faces, Ids)
 EigenFace.save('Recogniser/trainingDataEigan.xml')
-
-FisherFace.train(faces, Ids)
-FisherFace.save('Recogniser/trainingDataFisher.xml')
 
 LBPHFace.train(faces, Ids)
 LBPHFace.save('Recogniser/trainingDataLBPH.xml')
