4750f0ad48842cbc021f2e915c05bdb8751e6847
Arun <arunv4080@gmail.com>
2018-11-13 22:31:52 +0530

    version

--- create_dataset.py
+++ create_dataset.py
@@ -14,14 +14,15 @@ while 1:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)	
     for (x,y,w,h) in faces:
-	sampleNum=sampleNum+1
-	cv2.imwrite("dataset/user."+id+"."+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])        
-	cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        cv2.waitKey(500)
+        sampleNum=sampleNum+1
+        cv2.imwrite("dataset/user."+id+"."+str(sampleNum)+".jpg",cv2.resize(gray[y:y+h,x:x+w], (190,190)))
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    
     cv2.imshow("Face",img)
-    cv2.waitKey(0)
-    if(sampleNum>2000):
-	break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    if(sampleNum>200):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
